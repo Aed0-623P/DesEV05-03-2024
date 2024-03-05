af0cfd09c0d6b4239e13c01ac0c191b09e6ffa55
Aed0-623P <aedo.felipe95@gmail.com>
2024-03-05 13:20:15 -0300

    Primer commit, paso 1,2 y 3 funcionales

---
+++ level.py
@@ -0,0 +1,22 @@
+def choose_level(n_pregunta, p_level):
+    if n_pregunta >= 1 and n_pregunta < 3:
+        p_level = "basicas"
+    elif n_pregunta >= 3 and n_pregunta <= 4:
+        p_level = "intermedias"
+    elif n_pregunta > 4 and n_pregunta <= 9:
+        p_level = "avanzadas"
+    elif n_pregunta > 9:
+        p_level = "nivel inválido"
+
+    return p_level
+    # Construir lógica para escoger el nivel
+    ##################################################
+
+
+
+if __name__ == '__main__':
+    # verificar resultados
+    print(choose_level(2, 2)) # básicas
+    print(choose_level(3, 2)) # intermedias
+    print(choose_level(7, 2)) # avanzadas
+    print(choose_level(4, 3)) # intermedias

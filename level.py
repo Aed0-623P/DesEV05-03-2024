57cc33b577b4d26ef4b3670815c0e76f64c7f954
Aed0-623P <aedo.felipe95@gmail.com>
2024-03-05 16:12:24 -0300

    avanzando un poco, pero sigo con el key error en question.py

--- level.py
+++ level.py
@@ -1,21 +1,26 @@
 def choose_level(n_pregunta, p_level):
-    if n_pregunta >= 1 and n_pregunta < 3:
-        p_level = "basicas"
-    elif n_pregunta >= 3 and n_pregunta <= 4:
-        p_level = "intermedias"
-    elif n_pregunta > 4 and n_pregunta <= 9:
-        p_level = "avanzadas"
-    elif n_pregunta > 9:
-        p_level = "nivel inválido"
+    if p_level == 1:
+        return "basicas"
+    elif p_level == 2:
+        if n_pregunta >= 1 and n_pregunta < 3:
+            return "basicas"
+        elif n_pregunta >= 3 and n_pregunta <= 4:
+            return "intermedias"
+    elif p_level == 3:
+        if n_pregunta > 4 and n_pregunta <= 9:
+            return "avanzadas"
+        elif n_pregunta > 9:
+            return "expertas"
+    
 
-    return p_level
-    # Construir lógica para escoger el nivel
-    ##################################################
-
+    if n_pregunta >3 and p_level > 1:
+        return "shite"
+    
+    return "error" 
 
 if __name__ == "__main__":
-    # verificar resultados
-    print(choose_level(2, 2))  # básicas
+    # Verify results
+    print(choose_level(2, 1))  # basicas
     print(choose_level(3, 2))  # intermedias
-    print(choose_level(7, 2))  # avanzadas
-    print(choose_level(4, 3))  # intermedias
+    print(choose_level(7, 3))  # avanzadas
+    print(choose_level(4, 2))  # intermedias
